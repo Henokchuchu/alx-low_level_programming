0398a6a7d5670cfbf6d7258905307b589205fd5e
myUsername <myemail@gamil.com>
2023-02-20 03:20:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,10 @@ include <stdio.h>
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19"
 
-	return (1);
-}
+			return (1)
+
+
+
+}	
