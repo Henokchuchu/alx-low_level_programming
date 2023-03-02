8d900f0b26a0bbe8dedb0b9abebd56036d4e3f5a
root <myemail@gmail.com>
2023-03-02 06:38:41 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,7 +4,6 @@ int main(void)
 int n;
 int a[5];
 int *p;
-a[2] = 98
 a[2] = 1024;
 p = &n;
 /*
@@ -14,7 +13,8 @@ p = &n;
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
-*/
+*/ 
+;
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
