d9be44caee4978cfa55a6bc4313407395fd3c974
Henokchuchu <myemail@gmail.com>
2023-04-06 08:22:17 -0700

    new updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
 * get_bit - returns the value of a bit at an index in a decimal number
-* @n: to guess the number 
+* @n:number to guess
 * @index: index of the bit
 *
 * Return: to the true value
